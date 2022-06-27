d3e4a73890afeebc54dac4eeac982cf34881ff2e
danilo borges <danilosilva2017@hotmail.com>
2022-06-27 11:55:23 -0300

    Entendo o comando action

--- Operadores_Casting/Lambda_Expressions/Program.cs
+++ Operadores_Casting/Lambda_Expressions/Program.cs
@@ -8,19 +8,17 @@ namespace Lambda_Expressions
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-            numbers.Add(1);
-            numbers.Add(2);
-            numbers.Add(3);
-            numbers.Add(4);
-            numbers.Add(5);
-            numbers.Add(6);
-            numbers.Add(7);
-            numbers.Add(8);
-            numbers.Add(9);
-            numbers.Add(10);
+            List<string> namesList = new List<string>() { "Danilo", "Sueli", "João Borges", "Marcos", "Rosana", "Nayla", "Ysabela"};
 
-            var parsNumber = numbers.Where((num) => num > 3);
+            Action<string> names = (name) => Console.WriteLine(name);
+
+            foreach (var item in namesList)
+            {
+                names(item);
+            }
+
+            Console.ReadLine();
+
         }
 
         public static int Mutiplicar(int a)
