ad2922c30bf0d0904ac9c0b95b056774826eb3f0
danilo borges <danilosilva2017@hotmail.com>
2022-06-27 08:45:47 -0300

    Entendendo Func

---
+++ Operadores_Casting/Entendo_Func/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entendo_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> somar = Somar;
+
+            int result = Somar(2);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        public static string MensagemDeBomDia()
+        {
+            return "Bom dia Danilo!";
+        }
+
+        public static int Somar(int x)
+        {
+            return x + 2;
+        }
+    }
+}
