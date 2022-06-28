21ee22a6af59ce6ffdaf62d88324b314d1d1e8f8
danilo borges <danilosilva2017@hotmail.com>
2022-06-28 11:55:09 -0300

    Entendo como funciona um foreach por baixo dos panos

---
+++ Operadores_Casting/Enumerable_Vs_list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable_Vs_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var item = items.GetEnumerator(); // Enumera, define a quantidade de index do array e armazena
+
+            while (item.MoveNext())
+            {
+                Console.WriteLine(item.Current);
+            }
+
+            Console.ReadLine();
+        }    
+    }
+}
