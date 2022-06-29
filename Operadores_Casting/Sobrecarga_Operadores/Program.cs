e0dc827a085c5163ed9d2f2ed0a1a97d2850a21b
danilo borges <danilosilva2017@hotmail.com>
2022-06-29 08:23:35 -0300

    Entendi a sobrecarga de operadores

--- Operadores_Casting/Sobrecarga_Operadores/Program.cs
+++ Operadores_Casting/Sobrecarga_Operadores/Program.cs
@@ -7,24 +7,29 @@ namespace Sobrecarga_Operadores
     {
         static void Main(string[] args)
         {
-            var people1 = new People<string>() { "Danilo", "João"};
-            var people2 = new People<string>() { "Marcos", "Rosana" };
-            var result = people1 + people2;
+            // A seguir temos duas listas de pessoas
 
-            result.ForEach(element => Console.WriteLine(element));
+            var people1 = new People<string>() { "Danilo", "Marcos", "Rosana" };  // Primeira lista
+            var people2 = new People<string>() { "João", "Nayla", "Ysabella" };  // Segunda lista
+            var result = people1 + people2;  // Aqui precisamos somar, juntar as duas listas
+
+            result.ForEach((people) => Console.WriteLine(people));
 
             Console.ReadLine();
-        }  
-        
-        public class  People<T> : List<T>
+        }
+
+        public class People<T> : List<T>  // Criamos a classe People que herda da classe List
         {
-            public static People<T> operator +(People<T> p1, People<T> p2)
+            public static People<T> operator + (People<T> p1, People<T> p2)  // Aqui temos um método que vai retornar uma lista do "tipo" People. Este método possui dois parâmetros de entrada (duas listas)
             {
-                var people = new People<T>();
-                p1.ForEach(element => people.Add(element));
-                p1.ForEach(element => people.Add(element));
-                return people;
+                var people = new People<T>();  // Criamos uma nova variável para armazenar a junção das duas listas
+                p1.ForEach((element) => people.Add(element));  // O ForEach vai percorrer a primeira lista passada por parâmetro e armazenar na variável people
+                p2.ForEach((element) => people.Add(element));  
+                return people;  // Retorna a lista atualizada
+
+                // Após o retorno temos que percorrer novamente a lista de pessoas atualizada e exibir no console. Podemos realizar ade algumas formas utilizando um for, foreach ou ForEach funcional, este conforme o exemplom acima
             }
         }
+        
     }
 }
