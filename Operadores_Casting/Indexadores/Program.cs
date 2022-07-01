892ee557adefc7296923d5d90c110f5838113ac3
danilo borges <danilosilva2017@hotmail.com>
2022-07-01 13:33:53 -0300

    Trabalhando com indexadores

--- Operadores_Casting/Indexadores/Program.cs
+++ Operadores_Casting/Indexadores/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Indexadores
 {
@@ -10,11 +6,11 @@ namespace Indexadores
     {
         static void Main(string[] args)
         {
-            Temperaturas temp = new Temperaturas();
-            temp[1] = 11;
-            int t = temp[1];
+            Temperaturas_02 temp = new Temperaturas_02();
+            /*temp[1] = 11;
+            int t = temp[1];*/
 
-            Console.WriteLine(t);
+            Console.WriteLine(temp["Jan"]);
 
             Console.ReadLine();
         }
@@ -23,7 +19,7 @@ namespace Indexadores
         {
             int[] temperaturasDoAno = new int[] { 30, 31, 29, 27, 22, 16, 19, 23, 26, 27, 28 };
 
-            public int this [int mes]
+            public int this[int mes]
             {
                 get
                 {
@@ -32,6 +28,33 @@ namespace Indexadores
                 set
                 {
                     temperaturasDoAno[mes - 1] = value;
+                }
+            }
+        }
+
+        class Temperaturas_02
+        {
+            int[] temperaturasDoAno = new int[] { 30, 31, 29, 27, 22, 16, 19, 23, 26, 27, 28, 31 };
+            public int this[string mes]
+            {
+                get
+                {
+                    switch (mes) 
+                    {
+                        case "Jan": return temperaturasDoAno[0];
+                        case "Fev": return temperaturasDoAno[1];
+                        case "Mar": return temperaturasDoAno[2];
+                        case "Abr": return temperaturasDoAno[3];
+                        case "Mai": return temperaturasDoAno[4];
+                        case "Jun": return temperaturasDoAno[5];
+                        case "Jlh": return temperaturasDoAno[6];
+                        case "Ago": return temperaturasDoAno[7];
+                        case "Set": return temperaturasDoAno[8];
+                        case "Out": return temperaturasDoAno[9];
+                        case "Nov": return temperaturasDoAno[10];
+                        case "Dez": return temperaturasDoAno[11];
+                        default: return -1;
+                    }
                 }
             }
         }
